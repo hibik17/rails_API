01fd946ce2a137081e59c9b9d9b773d75f334d1d
hibik17 <hbtatsu@gmail.com>
2022-08-09 21:01:29 +0900

    search gem ransack install

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class BooksController < ApplicationController
   def index
-    @books = RakutenWebService::Books::Book.search(title: "Python")
+    @books = RakutenWebService::Books::Book.search(title: 'Python')
   end
 end
--- config/initializers/rakuten.rb
+++ config/initializers/rakuten.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 RakutenWebService.configure do |c|
   c.application_id = ENV['RAKUTEN_API_KEY']
   c.affiliate_id = ENV['RAKUTEN_AFLIATE_KEY']
